5258946f465c9952390856006db84dcae034c85c
mido <midohany910@gmail.com>
2024-08-07 02:47:54 +0300

    Handle empty url input

--- index.py
+++ index.py
@@ -1,7 +1,4 @@
-# NOTE AT the end of app try this from PyQt5 import QtGui,QtCore see if there's a difference in launch time
-# importing important modules
 from PyQt5.QtWidgets import *
-'''contains widgets that we'll use like QPushButton, QApplication'''
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from main import Ui_MainWindow
@@ -10,6 +7,7 @@ import sys
 import urllib.request
 from pytube import Playlist, YouTube
 import humanize
+
 
 # initializing a class that inherits its parameters and fncs from QMainWindow class
     # takes the variable containing the design as a 2nd parameter
@@ -37,10 +35,6 @@ class MainApp(QMainWindow, Ui_MainWindow):
         self.pushButton_10.clicked.connect(self.Get_playlist)
         self.pushButton_8.clicked.connect(self.Playlist_download)
 
-# NOTE when connecting a funcion with another don't use brackets in the 2nd fn.. just 
-# its name inside the 1st fn brackets, otherwise it will raise None Error.
-
-                    # MAIN APP FUNCTIONS
 ########################### FIRST TAB ##########################
     def Handel_Browse(self):
         save = QFileDialog.getSaveFileName(self, caption='Save as',directory='', filter='All Files (*.*)')
@@ -65,27 +59,25 @@ class MainApp(QMainWindow, Ui_MainWindow):
             QMessageBox.warning(self, 'Error :(', 'Download Failed !')
 
         self.progressBar.setValue(0)
-        # self.lineEdit.setText('')
-        # self.lineEdit_2.setText('')
-
 
 ####################### YouTube video tab ######################
 
     def Handel_browse_video(self):
         save = QFileDialog.getExistingDirectory(self, 'Save Video as')
         self.lineEdit_5.setText(save)
-        # the difference between this method and the one above in browse getSaveFileName 
-        # is that this takes just a path from user and returns a directory, while in the
-        # other we could name the file and choose extension.          
 
     def Get_Youtube_Video(self):
         self.comboBox.clear()
         url = self.lineEdit_6.text()
-        v = YouTube(url)
-        stream_list = v.streams.desc().filter(subtype='mp4', progressive=True)
-        for s in stream_list:
-            final = str((s.type, s.subtype,s.resolution, humanize.naturalsize(s.filesize))).replace("'",'')
-            self.comboBox.addItem(final)
+        try:
+            v = YouTube(url)
+            stream_list = v.streams.desc().filter(subtype='mp4', progressive=True)
+            for s in stream_list:
+                final = str((s.type, s.subtype,s.resolution, humanize.naturalsize(s.filesize))).replace("'",'')
+                self.comboBox.addItem(final)
+        except:
+            QMessageBox.warning(self, 'Invalid input!', "Please provide a valid Youtube url")
+            return None
 
     def Video_progress(self, chunk, file_handler, bytes_remaining):
         size = chunk.filesize
@@ -108,7 +100,6 @@ class MainApp(QMainWindow, Ui_MainWindow):
             QMessageBox.warning(self, 'Error!', 'Download Failed  :(')
         self.progressBar_2.setValue(0)
 
-
 ############################ Playlist Download #########################
 
     def Get_playlist(self):
@@ -128,11 +119,9 @@ class MainApp(QMainWindow, Ui_MainWindow):
         quality = self.comboBox_2.currentIndex()
         current_vid_num = 0
         chdir(save_location)
-        if path.exists(playlist.title):
-            chdir(playlist.title)     
-        else:
+        if not path.exists(playlist.title):
             mkdir(playlist.title)
-            chdir(playlist.title)
+        chdir(playlist.title)
         try:
             for video in range(playlist.length):
                 v = YouTube(playlist[video])
@@ -144,7 +133,7 @@ class MainApp(QMainWindow, Ui_MainWindow):
             QMessageBox.information(self, 'Success!','Video Downloaded completed :)')
         except:
             QMessageBox.warning(self, 'Error!', 'Download Failed  :(')
-    
+
         self.progressBar_2.setValue(0)
         self.lcdNumber.display(0)
 
@@ -158,9 +147,6 @@ def main():
     window = MainApp()
     window.show()
     app.exec()
-    '''creating an object (instance) of MainApp class
-        To show the app window
-        the app stays open (making an infinite loop)'''
 
 # App will start from here (main()fundtion)
 if __name__ == '__main__':
